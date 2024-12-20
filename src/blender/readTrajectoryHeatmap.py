75114b2b8880a31d671b1480a768609aaa38a5b5
Tyfor <timo@his4sports.de>
2024-12-20 11:02:01 +0100

    bugfix

--- src/blender/readTrajectoryHeatmap.py
+++ src/blender/readTrajectoryHeatmap.py
@@ -24,10 +24,10 @@ def readTrajectory(filename):
     return trajectory_data
 
 
-def generate_heatmap(particle_positions, resolution):
+def generate_heatmap(particle_positions, resolution, range):
     xCords = particle_positions[:, 0]               # we are plotting projected to the xy plane in this case
     yCords = particle_positions[:, 1]               # no fancy camera angle projections
-    heatmap, xedges, yedges = np.histogram2d(xCords, yCords, bins=resolution)
+    heatmap, xedges, yedges = np.histogram2d(xCords, yCords, bins=resolution, range=range, density=True)
 
     return heatmap
 
@@ -42,28 +42,38 @@ def generate_video(filename, trajectory,  resolution, zoom_margin=0.01):
     ax.set_xlabel("x-Position")
     ax.set_ylabel("y-Position")
 
-    ax.set_xlim(-1, 1)
-    ax.set_ylim(-1, 1)
+    #for constant color scale and zoom
+    x_min, x_max = -10, 10
+    y_min, y_max = -10, 10
+    particle_positions = np.asarray([*trajectory.get(20).values()])
+    heatmap = generate_heatmap(particle_positions, resolution, [[x_min, x_max], [y_min, y_max]])
+    heatmap_img.set_data(heatmap.T)  # transposed because imshow expects y first
+    # if frame == 1:
+    heatmap_img.set_clim(vmin=0, vmax=np.max(heatmap))
+
+
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
     def update(frame):
         # 2D array containing only the positions for the current frame
         if frame >= 1:
             particle_positions = np.asarray([*trajectory.get(frame).values()])
-
-            heatmap = generate_heatmap(particle_positions, resolution)
-            heatmap_img.set_data(heatmap.T) #transposed because imshow expects y first
-            #if frame == 1:
-            heatmap_img.set_clim(vmin=0, vmax=np.max(heatmap))  # Adjust color scale
-            heatmap_img.set_extent([-1, 1, -1, 1])
-
 
             """"# zoom to impoertant region (smart zoom)
             x_mean, y_mean = particle_positions[:, 0].mean(), particle_positions[:, 1].mean()
             x_std, y_std = particle_positions[:, 0].std(), particle_positions[:, 1].std()
             x_min, x_max = x_mean - 2 * x_std - zoom_margin, x_mean + 2 * x_std + zoom_margin
             y_min, y_max = y_mean - 2 * y_std - zoom_margin, y_mean + 2 * y_std + zoom_margin
-            heatmap_img.set_extent([x_min, x_max, y_min, y_max])
             ax.set_xlim(x_min, x_max)
             ax.set_ylim(y_min, y_max)"""
+            #constant zoom
+
+            heatmap = generate_heatmap(particle_positions, resolution, [[x_min, x_max], [y_min, y_max]])
+            heatmap_img.set_data(heatmap.T) #transposed because imshow expects y first
+            heatmap_img.set_extent([x_min, x_max, y_min, y_max])
+            #if frame == 1:
+            #heatmap_img.set_clim(vmin=0, vmax=np.max(heatmap))  # Adjust color scale
+
         return heatmap_img
 
     ani = animation.FuncAnimation(fig, update, frames=len(trajectory), interval=100)
