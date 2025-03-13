a6c856633613ba81e72a411c95b1307e10722206
Luca Dziarski <luca@dziarski.de>
2025-03-13 03:36:28 +0100

    implement volume rendering. (seems inefficient , not really usable)

--- src/main.py
+++ src/main.py
@@ -9,12 +9,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 t = PlummerSphere()
 
-samples = t.sample(int(2000))
+samples = t.sample(int(10000))
 # samples = [Point3D(p) for p in samples]
 # samples = np.array([x.to_list() for x in samples]).transpose()
 from utils.visualization import renderPointCloudInteractive, renderPointCloudDensityMap
 # renderPointCloudInteractive(samples, 0.01, 1.0, zoom=40)
-renderPointCloudDensityMap(samples, radius=1, dimensions=(100,100,100), focalPoint=(0,0,0))
+renderPointCloudDensityMap(samples, radius=0.5, dimensions=(60,60,60), focalPoint=(0,0,0))
 
 
 # # sim settins
--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -1,26 +1,42 @@
-from typing import Union
+from typing import Optional, Union
 from numpy import ndarray
 
-from vtkmodules.vtkCommonDataModel import vtkPolyData
 import vtkmodules.vtkInteractionStyle
 import vtkmodules.vtkRenderingOpenGL2
+import vtkmodules.vtkRenderingVolumeOpenGL2
+
+from vtkmodules.vtkCommonDataModel import vtkPolyData
 from vtkmodules.vtkCommonColor import vtkNamedColors
-from vtkmodules.vtkRenderingCore import vtkActor, vtkPointGaussianMapper, vtkPolyDataMapper, vtkRenderWindow, vtkRenderWindowInteractor, vtkRenderer, vtkVolume, vtkColorTransferFunction
+from vtkmodules.vtkRenderingCore import vtkActor, vtkPointGaussianMapper, vtkRenderWindow, vtkRenderWindowInteractor, vtkRenderer, vtkVolume, vtkColorTransferFunction
 from vtkmodules.vtkCommonDataModel import vtkPiecewiseFunction
 from vtkmodules.vtkCommonCore import vtkPoints
 from vtkmodules.util.numpy_support import numpy_to_vtk
 from vtkmodules.vtkImagingHybrid import vtkGaussianSplatter
-from vtkmodules.vtkRenderingVolume import vtkFixedPointVolumeRayCastMapper
-from vtkmodules.vtkRenderingVolumeOpenGL2 import vtkOpenGLRayCastImageDisplayHelper
+from vtkmodules.vtkRenderingVolumeOpenGL2 import vtkSmartVolumeMapper
 
-def renderPointCloudDensityMap(data: Union[ndarray, vtkPolyData], radius: float = 0.2, dimensions: tuple = (100,100,100), focalPoint: tuple = (0,0,0), zoom: float = 0):
+def renderPointCloudDensityMap(data: Union[ndarray, vtkPolyData], radius: float = 0.2, dimensions: tuple = (100,100,100), focalPoint: tuple = (0,0,0), zoom: float = 0, fixedWindow: Optional[tuple] = None):
     '''
-    Visualize a point cloud in as density Volume.
+    Visualize a point cloud in as Volume.
 
     Parameters
     ----------
     data: [ndarray, vtkPolyData]
         Point Cloud to visualize.
+    
+    radius: float
+        Radius of the gaussian splatter function.
+
+    dimensions: tuple[int]
+        Dimensions of the splatter volume.
+
+    focalPoint: tuple[float]
+        Focal point of the camera.
+
+    zoom: float
+        Zoom of the camera.
+
+    fixedWindow: tuple[float]|None
+        Optional fixed bounds of the volume.
     ''' 
     
     # convert numpy array to vtk data format
@@ -37,46 +53,38 @@ def renderPointCloudDensityMap(data: Union[ndarray, vtkPolyData], radius: float 
     splatter.SetSampleDimensions(*dimensions)
     splatter.SetRadius(radius)
     splatter.SetScalarWarping(False)
+    if fixedWindow is not None:
+        splatter.SetModelBounds(*fixedWindow)
 
-    #surface
-    from vtkmodules.vtkFiltersCore import vtkContourFilter
-    surface = vtkContourFilter()
-    surface.SetInputConnection(splatter.GetOutputPort())
-    surface.SetValue(0, 0.08)
-
-    mapper = vtkPolyDataMapper()
-    mapper.SetInputConnection(surface.GetOutputPort())
-    actor = vtkActor()
-    actor.SetMapper(mapper)
+    splatter.Update()
+    scalarRange = splatter.GetOutput().GetScalarRange()
 
     # mapper
-    # mapper = vtkFixedPointVolumeRayCastMapper()
-    # mapper.SetInputConnection(splatter.GetOutputPort())
-    #
-    #
-    # colorTransferFunction = vtkColorTransferFunction()
-    # colorTransferFunction.AddRGBPoint(0.0, 0.0, 0.0, 0.0)
-    # colorTransferFunction.AddRGBPoint(64.0, 1.0, 0.0, 0.0)
-    # colorTransferFunction.AddRGBPoint(128.0, 0.0, 0.0, 1.0)
-    # colorTransferFunction.AddRGBPoint(192.0, 0.0, 1.0, 0.0)
-    # colorTransferFunction.AddRGBPoint(255.0, 0.0, 0.2, 0.0)
-    #
-    # opacityTransferFunction = vtkPiecewiseFunction()
-    # opacityTransferFunction.AddPoint(20, 0.0)
-    # opacityTransferFunction.AddPoint(255, 0.2)
-    #
-    # # create volume
-    # volume = vtkVolume()
-    # volume.SetMapper(mapper)
-    # volume.GetProperty().SetColor(colorTransferFunction)
-    # volume.GetProperty().SetScalarOpacity(opacityTransferFunction)
+    mapper = vtkSmartVolumeMapper() 
+    # mapper.SetSampleDistance(0.05)
+    mapper.SetInputConnection(splatter.GetOutputPort())
+    mapper.SetBlendModeToComposite()
+
+    colorTransferFunction = vtkColorTransferFunction()
+    colorTransferFunction.AddRGBPoint(scalarRange[0], 0.0, 0.0, 1.0)   # Blue at min
+    colorTransferFunction.AddRGBPoint(scalarRange[1]/2, 1.0, 1.0, 1.0)   # White in middle
+    colorTransferFunction.AddRGBPoint(scalarRange[1], 1.0, 0.0, 0.0)   # Red at max
+
+    opacityTransferFunction = vtkPiecewiseFunction()
+    opacityTransferFunction.AddPoint(scalarRange[0], 0.001)
+    opacityTransferFunction.AddPoint(scalarRange[1]/2, 0.01)
+    opacityTransferFunction.AddPoint(scalarRange[1], 1.0)
+
+    # create volume
+    volume = vtkVolume()
+    volume.SetMapper(mapper)
+    volume.GetProperty().SetColor(colorTransferFunction)
+    volume.GetProperty().SetScalarOpacity(opacityTransferFunction)
 
     # renderer and window
     ren = vtkRenderer()
-    # ren.AddVolume(volume)
-    ren.AddActor(actor)
-    # ren.SetBackground(0,0,0)
-    ren.SetBackground(vtkNamedColors().GetColor3d('Wheat'))
+    ren.AddVolume(volume)
+    ren.SetBackground(0,0,0)
 
     renWin = vtkRenderWindow()
     renWin.AddRenderer(ren)
@@ -97,7 +105,7 @@ def renderPointCloudDensityMap(data: Union[ndarray, vtkPolyData], radius: float 
     iren.Start()
 
 
-def renderPointCloudInteractive(data: Union[ndarray, vtkPolyData], scaleFactor: float = 0.2, opacity: float = 0.2, focalPoint: tuple = (0,0,0), zoom: float = 0) -> None:
+def renderPointCloudInteractive(data: Union[ndarray, vtkPolyData], scaleFactor: float = 0.2, opacity: float = 1.0, focalPoint: tuple = (0,0,0), zoom: float = 0) -> None:
     '''
     Visualize a point cloud in an interactive Window.
 
@@ -105,6 +113,18 @@ def renderPointCloudInteractive(data: Union[ndarray, vtkPolyData], scaleFactor: 
     ----------
     data: [ndarray, vtkPolyData]
         Point Cloud to visualize.
+    
+    scaleFactor: float
+        Scale of the gaussian blur.
+
+    opacity: float
+        Opacity of points.
+
+    focalPoint: tuple[float]
+        Focal point of the camera.
+
+    zoom: float
+        Zoom of the camera.
     ''' 
     
     # convert numpy array to vtk data format
