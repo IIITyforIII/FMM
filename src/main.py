fc1d936f2c6adb225288256bb386d67a9ac8f1bf
Hans Rübberdt <hans.ruebberdt@web.de>
2025-03-14 15:18:04 +0100

    added dynamically build octtree for better performance

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from geolib.oct_tree import OctTreeCell
 from geolib.coordinates import Point3D
+from oct_tree import OctDynamicCell
 from physlib.entities import Particle
 from physlib.densityModels import UniformBox
 from os import write
@@ -16,7 +17,6 @@ import numpy as np
 # # renderPointCloudInteractive(samples, scaleFactor=0.01, zoom=40)
 # renderPointCloudDensityMap(samples, radius=0.2, dimensions=(100,100,100), focalPoint=(0,0,0))
 
-
 # sim settins
 num_particles = 1500
 domain_width = 1024
@@ -28,10 +28,13 @@ p = 4
 m = 3
 n = 3
 
+# for k in range(p - n + 1):
+#     for l in range(-k, k + 1, 1):
+#         print("M: " + str(abs(l)) + "  N:" + str(k))
 
 # init
 particles = [Particle(Point3D(p), Polar3D(0, 0, 0)) for p in UniformBox(domain_center, domain_width, domain_width, domain_width).sample(num_particles)]
-oct_tree = OctTreeCell(domain_center, domain_width, 2)
+oct_tree = OctDynamicCell(domain_center, domain_width, 8)
 oct_tree.insert_particles(particles)
 
 
