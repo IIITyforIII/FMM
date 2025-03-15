807e19f96ca4b8bacbce905ed9419fb87c0cc189
Luca Dziarski <luca@dziarski.de>
2025-03-15 14:13:48 +0100

    start direct summation

---
+++ src/simlib/directSummation.py
@@ -0,0 +1,8 @@
+import jax.numpy as jnp
+
+class nbodyDirectSimulator():
+    '''
+    Simulator for a n-body particle simulation using direct summation.
+    '''
+    def __init__(self) -> None:
+        pass
