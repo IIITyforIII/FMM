d07f45f8704bccc9089baef60e93722bc4fdb4a4
Luca Dziarski <luca@dziarski.de>
2025-03-19 03:47:14 +0100

    add velocity sampling to plummersphere, add direct summation main file.

--- src/physlib/densityModels.py
+++ src/physlib/densityModels.py
@@ -6,14 +6,20 @@ Convert to required units if needed.
 TODO: Right now only assuming particles of equal mass. (numerical and implementation simplicisity)
 """
 from typing import Union, List
+from jax.typing import ArrayLike
 from typing_extensions import override
 
 from geolib.coordinates import Point3D, Polar3D
 import numpy as np
 
+from abc import ABC, abstractmethod
+class densityModel(ABC):
+    @abstractmethod
+    def sample(self):
+        pass
+    
 
-
-class UniformBox():
+class UniformBox(densityModel):
     """
     A bounding Box where the mass is uniformly distributed -> random sampling (assuming equal mass bodies)
     """
@@ -35,11 +41,11 @@ class UniformBox():
         """Sample points according to density model."""
         return np.array([Point3D(p).to_Polar3D() for p in self.sample(n)])
 
-    def sample(self, n: int = 1) -> np.ndarray:
+    def sample(self, n: int = 1) -> np.ndarray: # pyright: ignore
         """Sample points according to density model."""
         return self.center.to_list() + self.__rng.uniform(self.__lbound, self.__ubound, (n,3))
 
-class UniformSphere():
+class UniformSphere(densityModel):
     """
     A bounding Sphere where the mass is unformly distributed -> random sampling (assuming equally heavy bodies)
     """
@@ -72,7 +78,7 @@ class UniformSphere():
         samples = np.array([self._sample_r(n), np.arccos(samples[0]), samples[1]]).transpose()
         return np.array([Polar3D(x) + self.center for x in samples])
 
-    def sample(self, n: int = 1) -> np.ndarray:
+    def sample(self, n: int = 1) -> np.ndarray: # pyright: ignore
         """Sample points according to density model."""
         return np.array([x.to_list() for x in self.samplePoint3D(n)])
 
@@ -84,7 +90,8 @@ class UniformSphere():
         
 class PlummerSphere(UniformSphere):
     """
-    A Sphere following the Plummer Model (G=1, M=1, a=1)
+    A Sphere following the Plummer Model (G=1, M=1, a=1).
+    Samples can be scaled manually.
     """
     def __init__(self, center: Union[Point3D, Polar3D, List[float]] = Point3D(0, 0, 0)) -> None:
         super().__init__(center, 1)
@@ -93,3 +100,27 @@ class PlummerSphere(UniformSphere):
     def _sample_r(self, n: int = 1) -> np.ndarray:
         samples = self._rng.uniform(0,1,n)
         return  np.power(np.power(samples, -2/3) - 1, -1/2)
+
+    def sample_vel(self, radius: ArrayLike):
+        '''Sample velocities according to radii (Neumann's rejection method).'''
+        escape_vel = lambda r: np.sqrt(2)*np.power((1+np.power(r,2)), -1/4)
+        g = lambda q: np.power(q,2)*np.power((1 - np.power(q,2)),7/2)
+
+        radius = np.array(radius)
+        vel = []
+
+        for r in radius:
+            sample = self._rng.uniform([0,0], [1,1], 1)
+            while 0.1 * sample[1] >= g(sample[0]):
+                sample = self._rng.uniform([0,0], [1,1], 1)
+            q = sample[0]
+            v = q*escape_vel(r)
+
+            sample = self._rng.uniform([0,0], [1,1], 1)
+            w = (1-2*sample[0]) * v
+            u = np.sqrt(np.power(v,2) - np.power(w,2)) * np.cos(2*np.pi*sample[1])
+            v = np.sqrt(np.power(v,2) - np.power(w,2)) * np.sin(2*np.pi*sample[1])
+
+            vel.append([u,v,w])
+
+        return np.array(vel)
